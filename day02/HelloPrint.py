c30b81496ecf5bc3cde4b9a6416901d44c56b52b
bobdfm0126 <bob940126bob@gmail.com>
2021-07-15 19:23:16 +0800

    Python test

---
+++ day02/HelloPrint.py
@@ -0,0 +1,13 @@
+name = "height"
+value = 170
+print(name, value) # 中間預設使用空白隔開
+print(name, value, sep="=") # 中間使用 = 隔開
+
+height = "身高"
+h = 170
+weight = "體重"
+w = 60
+print(height, h, sep="=", end="&") #使用& 符號解委
+print(weight, w, sep="=") #使用預設換行符號結尾
+
+
