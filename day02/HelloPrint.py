53bc4ca245def309f8c68b6c979e1832e9b4e1bb
bobdfm0126 <bob940126bob@gmail.com>
2021-07-15 19:32:31 +0800

    Python test

--- day02/HelloPrint.py
+++ day02/HelloPrint.py
@@ -10,4 +10,29 @@ w = 60
 print(height, h, sep="=", end="&") #使用& 符號解委
 print(weight, w, sep="=") #使用預設換行符號結尾
 
+print("--------------------")
+# 匯率列表
+symbol1 = "2330.TW"
+price1 = 650
+amount1 = 4500
+total1 = price1 * amount1
+
+symbol2 = "1101.TW"
+price2 = 53
+amount2 = 1200
+total2 = price2 * amount2
+# 資料對齊
+print("--------------------")
+print(symbol1, price1, amount1, total1)
+print(symbol2, price2, amount2, total2)
+print("--------------------")
+print("%4d" % 7)
+print("%4d" % 77)
+print("%4d" % 777)
+print("%4d" % 7777)
+print("%-4d" % 7)
+print("%-4d" % 77)
+print("%-4d" % 777)
+print("%-4d" % 7777)
+
 
