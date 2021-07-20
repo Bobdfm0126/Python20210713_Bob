75787fb1a8e3b4c7567eacba7e94bec5060ecbad
bobdfm0126 <bob940126bob@gmail.com>
2021-07-20 20:37:56 +0800

    Python test

--- day03/ForLoopDemo3.py
+++ day03/ForLoopDemo3.py
@@ -1,9 +1,10 @@
 # 99 乘法表
 # 雙重迴圈
-
+import time
 
 for x in range(1, 10):
     for y in range(1, 10):
         sum = x*y
         print("%d x %d = %d" % (x, y, sum), end ="\t")
+        time.sleep(0.2)
     print()
