0084b0e2a0606d431e244012d34bab3b980988d9
bobdfm0126 <bob940126bob@gmail.com>
2021-07-27 18:18:58 +0800

    Python test

---
+++ lab/SingleGuessNumber.py
@@ -0,0 +1,27 @@
+'''
+由系統產生 1~99 之間的亂數
+令 ans = 77
+=>
+
+'''
+'''
+使用者猜 77
+對了 !
+'''
+import random
+
+ans = random.randit(1, 99)
+min, max = 0, 100
+
+while True:
+    # User 猜
+    guess = input('請在%d~%d 間猜一個數字:' % (min, max))
+    guess = int(guess) #字串轉數字
+    # 判斷是否答對 ?
+    if guess < ans:
+        min = guess
+    elif guess > ans:
+        max = guess
+    else:
+        print('答對了')
+        break  # 迴圈結束
