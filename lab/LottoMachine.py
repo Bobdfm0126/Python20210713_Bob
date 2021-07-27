6358d2a92caf19f319bb78a6fa5f617fd861ea8a
bobdfm0126 <bob940126bob@gmail.com>
2021-07-27 20:39:49 +0800

    Python test

--- lab/LottoMachine.py
+++ lab/LottoMachine.py
@@ -1,6 +1,53 @@
 '''
 Lotto 機器
 可以提供重複或不重複的彩球
-例如 : 輸入一 --> 今彩 539 , 1~39 之間取不重複的 5 個彩球
-      輸入二 --> 四星彩 : 4 個 0~9 可以重複的數字
+例如 : 輸入1 -> 開獎今彩539: 1~39 之間取不重複的 5 個彩球
+      輸入2 -> 開獎四星彩 : 4 個 0~9 可以重複的數字
+      輸入0 -> Exit
 '''
+import random
+import sys
+
+def menu():
+    print('------------')
+    print('1. 今彩539')
+    print('2. 四星彩')
+    print('0. Exit')
+    print('------------')
+
+def userChoice():
+    choice = int(input('請選擇: '))
+    return choice
+
+def lotto539():
+    lotto = set()
+    while len(lotto) < 5:
+        lotto.add(random.randint(1, 39))
+
+    print('539:', lotto)
+def lotto4Stars():
+    lotto = []
+    for x in range(4):
+        lotto.append(random.randint(0, 9))
+    print('四星彩:', lotto)
+
+if __name__ == '__main__':
+    while True:
+        # Lotto 開獎 menu()
+        menu()
+        # User 選擇
+        choice = userChoice()
+        # 判斷選擇
+        if choice == 1:
+            lotto539()
+        elif choice == 2:
+            lotto4Stars()
+        elif choice == 0:
+            print('離開程式')
+            break
+        else:
+            print('輸入錯誤，請重新輸入 !')
+            continue
+
+        print('Enter...')
+        sys.stdin.read(1)
