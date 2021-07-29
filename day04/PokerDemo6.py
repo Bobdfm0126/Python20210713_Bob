e7529af912e47d600f8f1539a20bcf0bdd1bdb82
bobdfm0126 <bob940126bob@gmail.com>
2021-07-29 18:56:44 +0800

    Python test

--- day04/PokerDemo6.py
+++ day04/PokerDemo6.py
@@ -7,12 +7,17 @@ if __name__ == '__main__':
     balance = 100
     while True:
         print('balance:', balance)
+        if balance == 0:
+            print('你沒錢了，請離開遊戲...')
+            break;
+
         bet = int(input('請下注(0:離開): '))
 
-        if bet == 0 and balance == 0:
+        if bet == 0:
+            print('離開遊戲...')
             break;
 
         result = poker.playGame()
-        print(result)
+        #print(result)
         balance = balance + (result * bet)
-        print(balance)
+        #print(balance)
