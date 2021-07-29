21a3a3036a8675fd7518f302553b249e695db89a
bobdfm0126 <bob940126bob@gmail.com>
2021-07-29 19:05:55 +0800

    Python test

--- day04/PokerDemo6.py
+++ day04/PokerDemo6.py
@@ -1,6 +1,7 @@
 # 10點半對戰
 # 今天你有 100 元
 # 可以自由下注
+# 下注金額不可以超過 balance and<0, 寫完+6
 import day04.PokerDemo5 as poker
 
 if __name__ == '__main__':
@@ -13,7 +14,11 @@ if __name__ == '__main__':
 
         bet = int(input('請下注(0:離開): '))
 
-        if bet == 0:
+        if bet > balance or bet < 0:
+            print('下注金額不可以超過 balance,或是 < 0')
+            continue;
+
+        elif bet == 0:
             print('離開遊戲...')
             break;
 
