1fca92be5cedc769b80a46a96a0a586fe376c2cd
bobdfm0126 <bob940126bob@gmail.com>
2021-08-03 19:14:35 +0800

    Python test

--- day07/Youbike_Demo.py
+++ day07/Youbike_Demo.py
@@ -4,12 +4,18 @@ file = open('桃園公共自行車即時服務資料.json', 'r', encoding='utf-8
 data = file.read()
 youbike = json.loads(data)
 #print(youbike)
-print(youbike['retVal']['2001'])
-print(youbike['retVal']['2001']['sna'])
-print(youbike['retVal']['2001']['tot'])
-print(youbike['retVal']['2001']['sbi'])
-print(youbike['retVal']['2001']['bemp'])
-print(youbike['retVal']['2001']['lat'])
-print(youbike['retVal']['2001']['lng'])
+no = 3003
+sno = str(no)
+try:
+    print(youbike['retVal'][sno])
+    print(youbike['retVal'][sno]['sno'])
+    print(youbike['retVal'][sno]['sna'])
+    print(youbike['retVal'][sno]['tot'])
+    print(youbike['retVal'][sno]['sbi'])
+    print(youbike['retVal'][sno]['bemp'])
+    print(youbike['retVal'][sno]['lat'])
+    print(youbike['retVal'][sno]['lng'])
+except:
+    print('無此站台: %s' % sno)
 # 我所在的經緯度  24.992813597959195, 121.29529097506138 桃園市桃園區法治路20號
 print(u.distance(24.992813597959195, 121.29529097506138, 24.968128, 121.194666))
