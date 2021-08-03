2fe459bcb90b6505ce063c0c0e260fca088d9464
bobdfm0126 <bob940126bob@gmail.com>
2021-08-03 19:01:57 +0800

    Python test

--- day07/Youbike_Demo.py
+++ day07/Youbike_Demo.py
@@ -8,4 +8,6 @@ print(youbike['retVal']['2001']['sna'])
 print(youbike['retVal']['2001']['tot'])
 print(youbike['retVal']['2001']['sbi'])
 print(youbike['retVal']['2001']['bemp'])
+print(youbike['retVal']['2001']['lat'])
+print(youbike['retVal']['2001']['lng'])
 # 我所在的經緯度  24.992813597959195, 121.29529097506138 桃園市桃園區法治路20號
