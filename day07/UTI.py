14b26ba895e27810a287c622620246b2de5a6dba
bobdfm0126 <bob940126bob@gmail.com>
2021-08-03 19:06:26 +0800

    Python test

--- day07/UTI.py
+++ day07/UTI.py
@@ -7,7 +7,7 @@ def distance(lat1, lng1, lat2, lng2):
     dlng = lng2 - lng1
     dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlng/2)**2
-    c = 2* asin(sqrt(a))
+    c = 2 * asin(sqrt(a))
     r = 6371 # 地球半徑
     km = c * r
     m = km * 1000
--- day07/Youbike_Demo.py
+++ day07/Youbike_Demo.py
@@ -1,4 +1,5 @@
 import json
+import day07.UTI as u
 file = open('桃園公共自行車即時服務資料.json', 'r', encoding='utf-8')
 data = file.read()
 youbike = json.loads(data)
@@ -11,3 +12,4 @@ print(youbike['retVal']['2001']['bemp'])
 print(youbike['retVal']['2001']['lat'])
 print(youbike['retVal']['2001']['lng'])
 # 我所在的經緯度  24.992813597959195, 121.29529097506138 桃園市桃園區法治路20號
+print(u.distance(24.992813597959195, 121.29529097506138, 24.968128, 121.194666))
