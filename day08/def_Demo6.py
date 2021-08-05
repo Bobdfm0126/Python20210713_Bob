25bc56ca6e4e063492647335dad12e74a8239a32
bobdfm0126 <bob940126bob@gmail.com>
2021-08-05 18:55:01 +0800

    Python test

---
+++ day08/def_Demo6.py
@@ -0,0 +1,16 @@
+# 列印出每一科的成績，並且累計季算總分
+def printAndSum(subject, score):
+    global sum
+    sum = sum + score
+    print(subject, '=', score)
+
+if __name__ == '__main__':
+    sum = 0
+    printAndSum('國文', 100)
+    print(sum)
+    printAndSum('數學', 90)
+    print(sum)
+    printAndSum('英文', 80)
+    print(sum)
+    print('-----------')
+    print('總分 =', sum)
