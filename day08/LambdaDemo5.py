5a126608ae4ae8a6c8eb6c32d328b8ffc8e6feaa
bobdfm0126 <bob940126bob@gmail.com>
2021-08-10 18:17:07 +0800

    Python test

--- day08/LambdaDemo5.py
+++ day08/LambdaDemo5.py
@@ -2,11 +2,11 @@ id = 'A123456789'
 sex = id[1]  # 1: 男生, 2: 女生
 
 printSex = {  # dict 結構
-    "1": lambda n: print("男生", n),
-    "2": lambda n: print("女生", n)
+    "1": lambda: print("男生"),
+    "2": lambda: print("女生")
 }
 
-printSex.get(sex)()  # 最後要加上()， 因為是函式
+printSex.get(sex)()  # 最後要加上(), 因為是函式
 
-SexObj = printSex.get(sex)
-SexObj()
+sexObj = printSex.get(sex)
+sexObj()
