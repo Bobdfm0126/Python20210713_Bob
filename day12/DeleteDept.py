af8542973510033b4cb01b5c459baea73b586802
bobdfm0126 <bob940126bob@gmail.com>
2021-08-19 18:49:23 +0800

    Python test

---
+++ day12/DeleteDept.py
@@ -0,0 +1,23 @@
+import sqlite3
+# 刪除一個部門
+
+conn = sqlite3.connect('emp.db')
+cursor = conn.cursor()
+
+# 若要刪除　dept_id = 2
+# 先確認 employees 有無使用
+dept_id = 2
+sql = "select count(*) as count from employees where dept_id = %d" % (2)
+cursor.execute(sql)
+count = cursor.fetchone()
+print(count, count[0])
+if count[0] == 0:
+    sql = "Delete from departments where dept_id = %d" % (dept_id)
+    cursor.execute(sql)
+    conn.execute(sql)
+    conn.commit()
+    print('dept_id:', dept_id, 'Delete OK')
+else:
+    print('dept_id:', dept_id, '不可刪除')
+# sql = "Delete from departments where dept_id = %d" % (5)
+conn.close()
